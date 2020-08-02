f824cd2236c47058312d71dad9a7f6e19fe1f832
Mark <anonymous2438@gmail.com>
2020-08-03 06:34:38 +0700

    Add comments for linked-list's methods

--- LinkedList/Implementation/linked-list.h
+++ LinkedList/Implementation/linked-list.h
@@ -13,8 +13,19 @@ extern const struct LinkedListClass
 {
     // init new linked-list
     struct LinkedList* (*newList)();
+    // travel through each node in linked-list and print out the value
     void(*travel)(struct LinkedList* this);
+    // deallocate all nodes in linked-list and the list itself
     int (*freeList)(struct LinkedList* this);
+    // push an item into linked-list
     int (*push)(struct LinkedList* this, int data);
+    // pop an item out of linked-list
+    struct ListNode (*pop)(struct LinkedList* this);
+    // Add a node at the start of the linked-list
+    int (*unshift)(struct LinkedList* this, int data);
+    // insert a node at `index` in linked-list then returns new linked-list's length
+    int (*insert)(struct LinkedList* this, int index, int data);
+    // remove a node at the start of the linked-list then returns that node
+    struct ListNode (*shift)(struct LinkedList* this);
 } LinkedList;
 #endif
